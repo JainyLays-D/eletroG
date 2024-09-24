03e5989e109ae955729aa8cd9c890d4aed37b834
Jainy Lays <jainylays@gmail.com>
2024-09-24 11:13:08 -0300

    Add files via upload
    24/09/2024

---
+++ novoJL.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+    int numero1 = 6;
+    int numero2 = 20;
+    int soma;
+    char letra = '1';
+    char nome[5]={"jainy"};
+    printf("%c \n",letra);
+    printf("%s",nome);
+    soma  = numero1 + numero2;
+    printf("%d",soma);
+}
