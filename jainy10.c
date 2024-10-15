239b6e2358e6b90ebebe975ed79795754f064963
Jainy Lays <jainylays@gmail.com>
2024-10-15 10:35:59 -0300

    Add files via upload

---
+++ jainy10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2, n3;
+
+    printf("Digite três números distintos: ");
+    scanf("%d %d %d", &n1, &n2, &n3);
+
+    if (n1 > n2) { int temp = n1; n1 = n2; n2 = temp; }
+    if (n1 > n3) { int temp = n1; n1 = n3; n3 = temp; }
+    if (n2 > n3) { int temp = n2; n2 = n3; n3 = temp; }
+
+    printf("Ordem crescente: %d, %d, %d\n", n1, n2, n3);
+    
+
+}
