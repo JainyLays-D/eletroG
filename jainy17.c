9d1633a021d5eaaeb791ccc08a6ddc2669a9414f
Jainy Lays <jainylays@gmail.com>
2024-11-18 09:02:15 -0300

    Delete jainy17.c

--- jainy17.c
+++
@@ -1,8 +0,0 @@
-/* ascii.c - exibe a tabela ASCII especifica do seu computador */ 
-#include <stdio.h> 
-int main(void) { 
- int c; 
- for(c=0; c<=255; c++) 
- printf("(%d,%c) ", c, c); 
- return 0; 
-}
