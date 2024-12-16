6383de6e571dac8cb242b314be3a037564e4c0e4
Jainy Lays <jainylays@gmail.com>
2024-12-16 12:03:40 -0300

    Add files via upload

---
+++ jainy27.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define principal int main()
+#define inicio {
+#define fim }
+#define pi 3.1415
+
+principal
+inicio
+
+float r = 10;
+float comp_circunferencia;
+comp_circunferencia = 2*pi*r;
+printf("%f", comp_circunferencia);
+
+fim;
+
